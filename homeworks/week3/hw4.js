06dd96f3f2d01370dbe54ed190a2c574381578ca
Ruo0613 <75151902+Ruo0613@users.noreply.github.com>
2021-05-26 21:07:22 +0800

    Update hw4.js

--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -10,36 +10,19 @@ rl.on('close', function() {
 solve(lines)
 })
 function solve(lines) {
-    let m = Number(lines[0])
-    for(let i=1;i<=m; i++){
-      let [a, b, k] = lines[i].split(' ')
-      console.log(compare(a, b, k))
+    let str = lines[0]
+    if(reverse(str) === str){
+        console.log('True')
     }
-  }
-function compare(a, b, k) {  //比大小
-    if (a === b) return "DRAW"
-    if (k == -1) {    //對調
-      let temp = a
-      a = b
-      b = temp
-    }  
-    if (a.length != b.length) {
-        if(a.length > b.length){
-            return "A"
-        }
-        else{
-            return "B"
-        }
+    else{
+        console.log('False')
     }
-    for (let i=0; i<a.length; i++) {
-        if (a[i] == b[i]) {
-            continue;
-        }
-        if(a[i] > b[i]){
-            return "A"
-        }
-        else{
-            return "B"
-        }
+}
+
+function reverse(str){
+    let result = ''
+    for(let i=str.length-1; i>=0; i--){
+        result += str[i]
     }
-  }
+    return result
+}
